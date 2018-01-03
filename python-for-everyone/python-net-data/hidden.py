67673d71f2a05439ef78d95e0a4e3007ca8176e1
alejanpi <alejandro.pimentel_rios@nokia.com>
2018-01-03 17:32:09 -0600

    Twitter test from python.

---
+++ python-for-everyone/python-net-data/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "G61JU6MhChxEOQa8h0bGPjdsO",
+            "consumer_secret": "",
+            "token_key": "38587077-AKg9Bwtt8LmDDdj3bkpP6MhjV8kXifIYWrRxo0fWz",
+            "token_secret": ""}
