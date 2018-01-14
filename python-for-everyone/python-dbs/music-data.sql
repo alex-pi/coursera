db3455929efed0a922671ab4612b10b688e273f9
Alejandro Pimentel <berserkpi@gmail.com>
2018-01-13 20:36:57 -0600

    Python dbs week3.

---
+++ python-for-everyone/python-dbs/music-data.sql
@@ -0,0 +1,5 @@
+insert into Artist (name) values ('Led Zeppelin');
+insert into Artist (name) values ('AC/DC');
+insert into Genre (name) values ('Rock');
+insert into Genre (name) values ('Metal');
+
